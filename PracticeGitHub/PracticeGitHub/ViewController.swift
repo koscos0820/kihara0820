46d2b98823787a51e408bed40ad1ab2e7198b292
kihara kosuke <kosuke-kihara@cimtech.co.jp>
2020-04-06 15:35:44 +0900

    print("sub")の記述を追加

--- PracticeGitHub/PracticeGitHub/ViewController.swift
+++ PracticeGitHub/PracticeGitHub/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("1,2,3")
         print("ABCGHI")
+        print("sub")
     }
 
 
